503c9ec8ec05a7a59e00e6a2b05a3856ee3c188d
Yannic Kimmel <ykimmel@charlotte.edu>
2025-09-09 15:44:15 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,20 @@
+function templateProcessor(template){
+    this.template = template;
+}
+
+templateProcessor.prototype.fillIn = function(dictionary){
+    return this.template.replace(/{{(.+?)}}/g, function(match, key){
+        return dictionary[key];
+    });
+};
+
+const t1 = "My name is {{name}} and I am from {{city}}";
+const p1 = new templateProcessor(t1);
+const dictionary = { name: "John", city: "New York" };
+
+console.log(p1.fillIn({name: "Bob"}));
+
+const t2 = " This is a test. ";
+const p2 = new templateProcessor(t2);
+console.log(p2.fillIn(dictionary));
 
