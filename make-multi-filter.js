051541502890552b90a9c05139c0ccfcd208523d
bsaund12 <bsaund12@charlotte.edu>
2025-09-09 15:44:54 -0400

    updated makemultifilter

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,1 +1,23 @@
+'use strict';
+function makeMultiFilter(originalArray) {
+    const arrayFilterer = function(filterCriteria = undefined, callback = undefined) {
+        if(arrayFilterer.originalArray === undefined) {
+            arrayFilterer.currentArray = originalArray;
+        }
 
+        if(Array.isArray(originalArray)) {
+            if((typeof filterCriteria === "function")) {
+                arrayFilterer.currentArray.filter(e => filterCriteria(e));
+        } else {
+            return arrayFilterer.currentArray;
+        }
+    }
+
+        if((typeof callback === "function")) {
+            callback.call(arrayFilterer.originalArray, arrayFilterer.currentArray);
+        }
+
+        return arrayFilterer;
+    };
+    return arrayFilterer;
+}
