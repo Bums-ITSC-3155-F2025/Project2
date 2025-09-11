b7a3ed6a364923a27574c0bdbbeef3e63e26f67c
bsaund12 <bsaund12@charlotte.edu>
2025-09-11 14:46:20 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,20 +1,20 @@
-function templateProcessor(template){
+function TemplateProcessor(template){
     this.template = template;
 }
 
-templateProcessor.prototype.fillIn = function(dictionary){
+TemplateProcessor.prototype.fillIn = function(dictionary){
     return this.template.replace(/{{(.+?)}}/g, function(match, key){
         return dictionary[key];
     });
 };
 
 const t1 = "My name is {{name}} and I am from {{city}}";
-const p1 = new templateProcessor(t1);
+const p1 = new TemplateProcessor(t1);
 const dictionary = { name: "John", city: "New York" };
 
 console.log(p1.fillIn({name: "Bob"}));
 
 const t2 = " This is a test. ";
-const p2 = new templateProcessor(t2);
+const p2 = new TemplateProcessor(t2);
 console.log(p2.fillIn(dictionary));
 
